2a5ae9fd00f42bc93e3abfcdab41cb0eefdee70c
maxodrom <mxdr@yandex.ru>
2017-05-14 22:22:43 +0300

    Some improvements.
    Signed-off-by: maxodrom <mxdr@yandex.ru>

--- src/widgets/BaseWidget.php
+++ src/widgets/BaseWidget.php
@@ -8,6 +8,7 @@
 namespace maxodrom\vk\widgets;
 
 use yii\base\Widget;
+use yii\web\View;
 use maxodrom\vk\widgets\assets\OpenApiAsset;
 
 /**
@@ -18,11 +19,33 @@ use maxodrom\vk\widgets\assets\OpenApiAsset;
 abstract class BaseWidget extends Widget
 {
     /**
+     * @var integer
+     */
+    public $apiId;
+
+
+    /**
      * @inheritdoc
      */
     public function init()
     {
-        OpenApiAsset::register($this->view);
+        $this->view->registerAssetBundle(OpenApiAsset::className(), View::POS_HEAD);
+        $this->registerOpenApiScript();
+
         parent::init();
+    }
+
+    /**
+     * Register VK Open API script using apiId.
+     */
+    public function registerOpenApiScript()
+    {
+        $this->view->registerJs(<<<JS
+            VK.init({
+                apiId: {$this->apiId},
+                onlyWidgets: true
+            });
+JS
+        , View::POS_HEAD);
     }
 }
