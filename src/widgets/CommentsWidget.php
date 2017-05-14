889c18fda2d598600ad0f8c71fc70333e83e1802
maxodrom <mxdr@yandex.ru>
2017-05-14 22:21:59 +0300

    Initial widget's variant.
    Signed-off-by: maxodrom <mxdr@yandex.ru>

---
+++ src/widgets/CommentsWidget.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @author Max Alexandrov <max@maxodrom.ru>
+ * @since 1.0
+ * @license MIT
+ */
+
+namespace maxodrom\vk\widgets;
+
+use yii\helpers\Html;
+use yii\helpers\Json;
+
+/**
+ * Class CommentsWidget
+ *
+ * @package maxodrom\vk\widgets
+ */
+class CommentsWidget extends BaseWidget
+{
+    const WIDGET_NAME = 'vk_comments';
+
+    /**
+     * @var array
+     */
+    public static $commentLimits = [5, 10, 15, 20];
+    /**
+     * @var int
+     */
+    public $commentsLimit = 10;
+    /**
+     * @var string
+     */
+    public $width = '';
+    /**
+     * @var string
+     */
+    private $containerId;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (!is_int($this->commentsLimit) || !in_array($this->commentsLimit, self::$commentLimits)) {
+            $this->commentsLimit = 10;
+        }
+        $this->containerId = self::WIDGET_NAME . '_' . $this->getId();
+
+        parent::init();
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $props = [
+            'limit' => $this->commentsLimit,
+            'attach' => '*',
+        ];
+
+        echo Html::tag(
+            'div',
+            '',
+            [
+                'id' => $this->containerId,
+            ]
+        );
+        echo Html::tag(
+            'script',
+            "VK.Widgets.Comments('" . $this->containerId . "', " . Json::encode($props) . ");",
+            [
+                'type' => 'text/javascript'
+            ]
+        );
+
+        parent::run();
+    }
+}
