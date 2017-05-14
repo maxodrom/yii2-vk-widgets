932628f324753468746ac2edde6ea6e65f16d41d
maxodrom <mxdr@yandex.ru>
2017-05-14 20:57:15 +0300

    Add BaseWidget class.
    Signed-off-by: maxodrom <mxdr@yandex.ru>

---
+++ src/widget/BaseWidget.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Max Alexandrov <max@maxodrom.ru>
+ * @since 1.0
+ * @license MIT
+ */
+
+namespace maxodrom\vk\widgets;
+
+use yii\base\Widget;
+
+/**
+ * Class BaseWidget
+ */
+abstract class BaseWidget extends Widget
+{
+
+}
